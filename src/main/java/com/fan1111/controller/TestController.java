f74b14de35740cfea25af7039002aa86abe3eb74
wyd288 <416700495@qq.com>
2019-03-16 21:13:48 +0800

    添加一个测试数据接口

---
+++ src/main/java/com/fan1111/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.fan1111.controller;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.fan1111.entity.User;
+import com.fan1111.service.user.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private UserService userService;
+
+
+    //测试访问接口
+    @RequestMapping(value="getusers",method= RequestMethod.GET)
+    @ResponseBody
+    public Object  getUsers(){
+        List<User> ulist = userService.getUserList(null,null);
+        return JSONArray.toJSONString(ulist);
+    }
+}
